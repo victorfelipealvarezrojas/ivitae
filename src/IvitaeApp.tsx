711b979df3f91e2af3893e64fe3643ea8941e52b
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-01-07 16:45:58 -0300

    deploy

--- src/IvitaeApp.tsx
+++ src/IvitaeApp.tsx
@@ -1,12 +1,12 @@
+import { Profile } from './Container/Home/Profile'
 import './IvitaeApp.stype.css'
 
 function Vitae() {
 
   return (
-    <>
-      <div>
-        <h1>IVitae</h1></div>
-    </>
+    <div>
+        <Profile/>
+    </div>
   )
 }
 
